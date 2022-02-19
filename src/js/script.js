0303b442d8ce8beacbf765dd5f1c0049eb70c85a
northvillagesatoshi <98567552+northvillagesatoshi@users.noreply.github.com>
2022-02-19 17:50:05 +0900

    [fix]to-top (#27)
    c-to-topが表示されていなかったので、修正しました。

--- src/js/script.js
+++ src/js/script.js
@@ -64,20 +64,20 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
   // スクロール後ヘッダースタイル変更
-  // var aboPos = $(".js-mv").offset().top;
+  var aboPos = $(".js-mv").offset().top;
 
-  // aboPos -= -130;
+  aboPos -= -130;
 
-  // $(window).scroll(function () {
+  $(window).scroll(function () {
 
-  //   if ($(window).scrollTop() > aboPos) {
-  //     $(".js-to-top").addClass("to-top__scroll");
-  //   } else {
-  //     $(".js-to-top").removeClass("to-top__scroll");
+    if ($(window).scrollTop() > aboPos) {
+      $(".js-to-top").addClass("to-top__scroll");
+    } else {
+      $(".js-to-top").removeClass("to-top__scroll");
 
-  //   }
+    }
 
-  // });
+  });
 
 
   var mySwiper = new Swiper('.swiper-container', {
